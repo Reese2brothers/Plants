b20c4a55515b71b9b3fc285df2d07df5a44aaaac
Reese <reese2brothers@gmail.com>
2024-05-14 15:25:40 +0300

    added edit screen code

--- app/src/main/java/com/tragulon/plants/screens/EditScreen.kt
+++ app/src/main/java/com/tragulon/plants/screens/EditScreen.kt
@@ -7,13 +7,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Done
@@ -43,10 +48,12 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImagePainter.State.Empty.painter
 import com.tragulon.plants.R
 import com.tragulon.plants.screens.viewmodels.EditViewModel
 import com.tragulon.plants.screens.viewmodels.FavouriteViewModel
@@ -54,8 +61,10 @@ import com.tragulon.plants.screens.viewmodels.FavouriteViewModel
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditScreen(mainplants : List<Int>) {
-    val texts = rememberSaveable { mutableStateOf("") }
+    val saveText = rememberSaveable { mutableStateOf("сохранить") }
     val imageResource = rememberSaveable { mutableStateOf(mainplants.random()) }
+    val imageSave = rememberSaveable { mutableStateOf(R.drawable.baseline_done_red) }
+    val saveTextColor = rememberSaveable { mutableStateOf(R.color.red) }
     val scope = rememberCoroutineScope()
     val viewModel: EditViewModel = hiltViewModel()
     val edits by viewModel.edits.collectAsState(initial = emptyList())
@@ -87,11 +96,12 @@ fun EditScreen(mainplants : List<Int>) {
             value = currentText.value, onValueChange = { newValue ->
                 currentText.value = newValue
                 viewModel.updateText(newValue)
+                imageSave.value = R.drawable.baseline_done_red
+                saveTextColor.value = R.color.red
             },
             keyboardActions = KeyboardActions(onDone = {
                 viewModel.addToEditEntity(currentText.value)
             }),
-            //keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
             modifier = Modifier.fillMaxSize(),
             textStyle = TextStyle(fontSize = 20.sp),
             label = {
@@ -109,24 +119,33 @@ fun EditScreen(mainplants : List<Int>) {
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text("Пишите здесь...", color = colorResource(id = R.color.orange))
-                    IconButton(onClick = { viewModel.addToEditEntity(currentText.value) }) {
-                        Icon(
-                            Icons.Default.Done,
-                            contentDescription = "Сохранить",
-                            tint = colorResource(id = R.color.statusBarColor),
-                        )
-                    }
-                    IconButton(onClick = { currentText.value = "" })
-                    {
-                    Icon(
-                        Icons.Default.Clear,
-                        contentDescription = "Очистить поле",
-                        tint = colorResource(id = R.color.statusBarColor),
-                    )
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.padding(end = 8.dp)
+                        ) {
+                            IconButton(onClick = { viewModel.addToEditEntity(currentText.value)
+                                imageSave.value = R.drawable.baseline_done_green
+                                saveText.value = "сохранено"
+                                saveTextColor.value = R.color.statusBarColor}) { Image(painter = painterResource(id = imageSave.value), contentDescription = "save",
+                                    modifier = Modifier.size(20.dp)) }
+                            Text(text = saveText.value, fontSize = 10.sp, fontWeight = FontWeight.Bold, color = colorResource(id = saveTextColor.value))
+                        }
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            IconButton(onClick = { currentText.value = "" }) {
+                                Icon(
+                                    Icons.Default.Clear,
+                                    contentDescription = "Очистить поле",
+                                    tint = colorResource(id = R.color.statusBarColor),
+                                )
+                            }
+                            Text(text = "очистить", fontSize = 10.sp, fontWeight = FontWeight.Bold, color = colorResource(id = R.color.statusBarColor))
+                        }
                     }
                 }
-
-            },
+            }
         )
     }
 }
